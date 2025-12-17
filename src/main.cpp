233874812f2b740306365d376d9fae76bf9b3169
stevehenny <stephen.henstrom@gmail.com>
2025-12-16 23:25:40 -0600

    adding CLI11

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,33 @@
 #include <CLI.hpp>
+#include <string>
 int main(int argc, char* argv[]) {
-  CLI::App cache_scope("", "cache_scope");
+  CLI::App app("", "cache_scope");
+
+  app.require_subcommand(1);
+
+  bool verbose = false;
+  app.add_flag("-v,--verbose", verbose, "Enable verbose debugging output");
+
+  std::string binary;
+  CLI::App* analyze =
+    app.add_subcommand("analyze", "Analyze cache behavior of a binary");
+
+  analyze->add_option("binary", binary, "Binary to analyze")
+    ->required()
+    ->check(CLI::ExistingFile);
+
+  analyze->callback([&]() {
+    // TODO: Add analyze callback here
+  });
+
+  std::string trace_file;
+  CLI::App* visualize =
+    app.add_subcommand("visualize", "Visualize cache trace output");
+
+  visualize->add_option("-t,--trace", trace_file, "Trace file");
+  visualize->callback([&]() {
+    // TODO: Add visualize callback here
+  });
 
   return 0;
 }
