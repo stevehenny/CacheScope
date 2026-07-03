7393edf94ea619bd8a9eb3b37fdbde13b0c6df4b
stevehenny <stephen.henstrom@gmail.com>
2026-07-02 23:47:11 -0700

    Revert "moving homeless functions into Utils namespace"
    This reverts commit b809689145d1f6df16461411eed67f3c4b6e901e.

--- include/runtime/PerfEventRecorder.hpp
+++ include/runtime/PerfEventRecorder.hpp
@@ -1,11 +1,9 @@
 #pragma once
 
 #include <optional>
-#include <functional>
 #include <string>
 #include <string_view>
 #include <vector>
-#include <sys/types.h>
 
 #include "common/Types.hpp"
 
@@ -26,18 +24,8 @@ struct PerfRecordResult {
 
 class PerfEventRecorder {
 public:
-  using MonitorUpdateCallback =
-    std::function<void(const std::vector<PerfSample>& samples,
-                       size_t new_samples, bool done)>;
-
-  PerfRecordResult record_binary(const std::string& binary,
-                                 const std::string& event_spec,
-                                 int sample_period, bool verbose);
-
-  PerfRecordResult record_pid(pid_t pid, const std::string& binary,
-                              const std::string& event_spec,
-                              int sample_period, bool verbose,
-                              MonitorUpdateCallback on_update = {});
-
+  PerfRecordResult record(const std::string& binary,
+                          const std::string& event_spec, int sample_period,
+                          bool verbose);
   static std::vector<std::string> split_event_spec(std::string_view spec);
 };
